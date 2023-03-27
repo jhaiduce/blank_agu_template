d054cf8701b250e1c3cb460796aa8832156f8295
John Haiducek <jhaiduce@gmail.com>
2023-03-27 17:37:10 -0400

    Add diffed paper target to CMake build

--- run-latexdiff.sh
+++ run-latexdiff.sh
@@ -1,1 +1,1 @@
-git latexdiff --no-cleanup --prepare 'echo Copying from '"$PWD"' to $PWD; rsync -avL --exclude=.git --include=\*/ --include=\*.pdf --include=tables/\*.tex --include=\*.ini --exclude=\* '"$PWD"'/ .' --ignore-makefile --latexmk $@
+git latexdiff --no-cleanup --prepare 'echo Copying from '"$PWD"' to $PWD; rsync -avL --exclude=.git --exclude=build --include=\*/ --include=\*.pdf --include=tables/\*.tex --include=\*.ini --exclude=\* '"$PWD"'/ .' --ignore-makefile --latexmk $@
